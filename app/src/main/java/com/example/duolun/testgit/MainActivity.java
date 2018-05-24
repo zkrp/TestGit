7bae65d2ffba71c52d2b31ac54423e8beac11dd5
pullnews <pullnews@sina.com>
2018-05-24 16:30:36 +0800

    添加注释

--- app/src/main/java/com/example/duolun/testgit/MainActivity.java
+++ app/src/main/java/com/example/duolun/testgit/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.duolun.testgit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// 添加注释
 public class MainActivity extends AppCompatActivity {
 
     @Override
